a4cbcb425575c85bacedf41a4d2a148ed4f2e5a0
aleCastro2001 <121947915+aleCastro2001@users.noreply.github.com>
2023-04-12 14:50:38 -0400

    guides section

---
+++ guias/guias.js
@@ -0,0 +1,18 @@
+const enlaces1=document.querySelector(".enlaces1");
+const clases1=enlaces1.classList;
+
+function desplegar1(){
+    clases1.toggle("mostrado")
+}
+
+const enlaces2=document.querySelector(".enlaces2");
+const clases2=enlaces2.classList;
+function desplegar2(){
+    clases2.toggle("mostrado")
+}
+
+const enlaces3=document.querySelector(".enlaces3");
+const clases3=enlaces3.classList;
+function desplegar3(){
+    clases3.toggle("mostrado")
+}
